48b405394c1ff050298a96ae9acd8577fe48dd70
Sebastian <shuertas981@gmail.com>
2023-03-13 22:41:09 -0500

    Agregando el atributo en privado

--- index.js
+++ index.js
@@ -20,30 +20,38 @@ class Cliente
 class CuentaCorriente
 {
     numero;
-    saldo;
+    #saldo;
     agencia;
 
     //creando un constructor
     constructor(){
-        this.saldo=0;
+        this.#saldo=0;
         this.numero='';
         this.agencia='';
+
     }
 
     
     //creando un metodo
     //deposito
     depositoEnCuenta(valor){
-        this.saldo += valor; //this te define la cuenta corriente actual, puede ser [1,2,3.....]
+        if(valor > 0){
+            this.#saldo += valor; //this te define la cuenta corriente actual, puede ser [1,2,3.....]
+        }
     }
     retiroEnCuenta(valor){
-        this.saldo -= valor;
+        if(valor<=this.#saldo){
+            this.#saldo -= valor;
+        }
+
     }
 }
 
 //Nueva Instancia
 cuentaDeLeonardo = new CuentaCorriente();
 cuentaDeLeonardo.depositoEnCuenta(100);
+cuentaDeLeonardo.numero='21312412';
+cuentaDeLeonardo.agencia=1001;
 console.log(cuentaDeLeonardo);
 cuentaDeLeonardo.retiroEnCuenta(50);
 console.log(cuentaDeLeonardo);
