747be1622da6a39fc130aa39b437553501e76600
Sebastian <shuertas981@gmail.com>
2023-03-13 20:12:48 -0500

    Creando clases y usandolas en listas

---
+++ index.js
@@ -0,0 +1,35 @@
+//cliente
+
+//creando clase cliente
+class Cliente
+{
+    nombreCliente;
+    dniCliente;
+    numeroCuenta;
+    saldoCuenta;
+    rucCliente;
+}
+//creando una constante que tome el valor del nuevo arreglo
+const cliente1= new Cliente();
+
+cliente1.nombreCliente='José';
+cliente1.dniCliente='123412';
+cliente1.numeroCuenta='232323';
+cliente1.saldoCuenta=5000;
+
+const cliente2= new Cliente();
+cliente2.nombreCliente='Leonardo';
+cliente2.dniCliente='13804050';
+cliente2.numeroCuenta='1234567890';
+cliente2.saldoCuenta=1000;
+
+const cliente3= new Cliente();
+cliente3.nombreCliente='Julia';
+cliente3.dniCliente='542342324';
+cliente3.numeroCuenta='342325231';
+cliente3.saldoCuenta=1000;
+//forma tradicional
+const nombreCliente='Leonardo';
+const dniCliente='13804050';
+const numeroCuenta='1234567890';
+const saldoCuenta =1000;
