04982b52280a305d0f96bfa2bc6b2041eebe46f1
Sebastian <shuertas981@gmail.com>
2023-03-23 06:55:53 -0500

    Poniendo un throw Error cuando quieran instanciar objetos de la clase cuenta

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import {Cliente} from './cliente.js';
 import {CuentaCorriente}from './cuentaCorriente.js';
 import {CuentaAhorro} from './CuentaAhorro.js';
+import {Cuenta} from "./Cuenta.js";
 
 const cliente =new Cliente('Leonardo','1242412','123445');
 const cliente2 =new Cliente('Maria', '1242512', '321414');
@@ -10,17 +11,6 @@ const cuentaDeLeonardo = new CuentaCorriente('Corriente',cliente,'1', '001');
 const cuentaDeMaria = new CuentaCorriente('Corriente',cliente2, '2', '002');
 
 const cuentaAhorroLeonardo= new CuentaAhorro(cliente, '9985', '001', 0);
-console.log(cuentaDeLeonardo);
 
-//operaciones sobre las cuentas
-
-console.log(cuentaDeLeonardo);
-cuentaDeLeonardo.depositoEnCuenta(150);
-console.log(cuentaDeLeonardo.verSaldo());
-cuentaDeLeonardo.retiroEnCuenta(70);
-console.log(cuentaDeLeonardo.verSaldo());
 console.log(cuentaAhorroLeonardo);
-cuentaAhorroLeonardo.depositoEnCuenta(200);
-console.log(cuentaAhorroLeonardo.verSaldo());
-cuentaAhorroLeonardo.retiroEnCuenta(120);
-console.log(cuentaAhorroLeonardo.verSaldo());
+console.log(cuentaDeMaria);
