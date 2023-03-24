d900ea3874fa71f1a8165e5cb3c0181d31e7df27
Sebastian <shuertas981@gmail.com>
2023-03-23 22:46:00 -0500

    Creando un sistema de Autenticacion para cliente y empleado

--- cliente.js
+++ cliente.js
@@ -14,11 +14,21 @@ export class Cliente
     nombreCliente;
     dniCliente;
     rucCliente;
+    #clave;
 
     constructor(nombreCliente, dniCliente, rucCliente){
         this.nombreCliente=nombreCliente;
         this.dniCliente=dniCliente;
         this.rucCliente=rucCliente;
+        this.#clave='';
 
     }
+
+    asignarClave(clave){
+        this.#clave=clave;
+     }
+
+     get clave(){
+        return this.#clave;
+     }
 }
--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 //importando una clase
 import {Cliente} from './cliente.js';
-import { CuentaAhorro } from './Cuentas/CuentaAhorro.js';
-import{CuentaCorriente} from './Cuentas/cuentaCorriente.js';
-import {Cuenta} from "./Cuentas/Cuenta.js";
-import {CuentaNomina} from './Cuentas/CuentaNomina.js';
 import { Empleado } from './Empleados/Empleado.js';
 import { Gerente } from './Empleados/Gerente.js';
 import { Director } from './Empleados/Director.js';
-
+import { SistemaAutenticacion } from './SistemaAutenticacion.js';
 const cliente =new Cliente('Leonardo','1242412','123445');
-const cliente2 =new Cliente('Maria', '1242512', '321414');
+cliente.asignarClave('1111');
+console.log(SistemaAutenticacion.login(cliente,'1111'));
+//const cliente2 =new Cliente('Maria', '1242512', '321414');
 
 const empleado =new Empleado('Juan Perez', '831297213', 2000);
+empleado.asignarClave('12345');
 const gerente = new Gerente('Pedro Rivas', '2312323132',17000);
 const director = new Director('Elena Moreno', '873218212', 150000);
 
-console.log(empleado.verBonificacion());
-console.log(gerente.verBonificacion());
-console.log(director.verBonificacion());
+console.log(SistemaAutenticacion.login(empleado,'12345'));
+
